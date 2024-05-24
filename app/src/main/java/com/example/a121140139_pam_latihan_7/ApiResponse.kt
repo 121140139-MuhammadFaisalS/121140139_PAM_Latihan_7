200c4439ebcbd32e18eef24aea605e0fc2c6b7d8
121140139-MuhammadFaisalS <162481555+121140139-MuhammadFaisalS@users.noreply.github.com>
2024-05-24 23:09:50 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/a121140139_pam_latihan_7/ApiResponse.kt
@@ -0,0 +1,6 @@
+package com.example.a121140139_pam_latihan_7
+
+
+class ApiResponse {
+    val data: List<User> = TODO()
+}
---
+++ app/src/main/java/com/example/a121140139_pam_latihan_7/User.kt
@@ -0,0 +1,9 @@
+package com.example.a121140139_pam_latihan_7
+
+data class User(
+    val id: Int,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val avatar: String
+)
